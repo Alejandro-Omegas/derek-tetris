f3c454e4258f0c8a4cf3311f9593b2e26e583620
Alejandro Arriola <aarriola.py@gmail.com>
2025-03-10 16:13:07 -0300

    attempts to fix out of bounds oddity

--- TetrisBasic.js
+++ TetrisBasic.js
@@ -1,10 +1,17 @@
 //#region Globals
+//#region Constants
+let STARTX = 4
+let STARTY = 0
+let DIRECTION = {
+    IDLE: 0, DOWN: 1, LEFT: 2, RIGHT: 3
+}
+//endregion
 let canvas
 let ctx
 let gBArrayHeight = 20
 let gBArrayWidth = 12
-let startX = 4
-let startY = 0
+let startX = STARTX
+let startY = STARTY
 let score = 0
 let level = 1
 let WinOrLose = "Playing"
@@ -18,9 +25,6 @@ let tetrominoColors = ['purple', 'cyan', 'blue', 'yellow', 'orange', 'green', 'r
 let currTetrominoColor
 let gameBoardArray = [...Array(gBArrayHeight)].map(e => Array(gBArrayWidth).fill(0))
 let stoppedShapeArray = [...Array(gBArrayHeight)].map(e => Array(gBArrayWidth).fill(0))
-let DIRECTION = {
-    IDLE: 0, DOWN: 1, LEFT: 2, RIGHT: 3
-}
 let direction
 //#endregion
 
@@ -72,7 +76,7 @@ function SetupCanvas() {
     ctx.strokeRect(8, 8, 280, 462)
 
     tetrisLogo = new Image(161, 54) //same size as your logo
-    tetrisLogo.onload = DrawTetrisLogo
+    tetrisLogo.onload = () => { ctx.drawImage(tetrisLogo, 300, 8, 161, 54) }
     tetrisLogo.src = "tetrislogo.png"
 
     ctx.fillStyle = 'black'
@@ -93,7 +97,7 @@ function SetupCanvas() {
     ctx.strokeRect(300, 366, 161, 104)
     ctx.font = '19px Arial'
     ctx.fillText("A : Move Left", 310, 388)
-    ctx.fillText("B : Move Right", 310, 413)
+    ctx.fillText("D : Move Right", 310, 413)
     ctx.fillText("S : Move Down", 310, 438)
     ctx.fillText("E : Rotate Right", 310, 463)
 
@@ -105,31 +109,49 @@ function SetupCanvas() {
     DrawTetromino()
 }
 
-function DrawTetrisLogo() {
-    ctx.drawImage(tetrisLogo, 300, 8, 161, 54)
-}
-
 function DrawTetromino() {
+    if (!currTetromino) {
+        CreateTetromino()
+        return
+    }
+
     for (let i = 0; i < currTetromino.length; i++) {
         let x = currTetromino[i][0] + startX
         let y = currTetromino[i][1] + startY
-        gameBoardArray[x][y] = 1
-        let coorX = coordinateArray[x][y].x
-        let coorY = coordinateArray[x][y].y
-        ctx.fillStyle = currTetrominoColor
-        ctx.fillRect(coorX, coorY, 21, 21)
+
+        if (x >= 0 && y >= 0) { //only draws if the block is not out of bounds
+            gameBoardArray[x][y] = 1
+
+            if (coordinateArray[x][y]) { //only draws the block if it is not out of bounds
+                let coorX = coordinateArray[x][y].x
+                let coorY = coordinateArray[x][y].y
+                ctx.fillStyle = currTetrominoColor
+                ctx.fillRect(coorX, coorY, 21, 21)
+            }
+        }
     }
 }
 
 function DeleteTetromino() {
+    if (!currTetromino) {
+        DrawTetromino()
+        return
+    }
+
     for (let i = 0; i < currTetromino.length; i++) {
         let x = currTetromino[i][0] + startX
         let y = currTetromino[i][1] + startY
-        gameBoardArray[x][y] = 0
-        let coorX = coordinateArray[x][y].x
-        let coorY = coordinateArray[x][y].y
-        ctx.fillStyle = 'white'
-        ctx.fillRect(coorX, coorY, 21, 21)
+
+        if (x >= 0 && y >= 0) {
+            gameBoardArray[x][y] = 0
+
+            if (coordinateArray[x][y]) {
+                let coorX = coordinateArray[x][y].x
+                let coorY = coordinateArray[x][y].y
+                ctx.fillStyle = 'white'
+                ctx.fillRect(coorX, coorY, 21, 21)
+            }
+        }
     }
 }
 
@@ -191,6 +213,13 @@ function MoveTetrominoDown() {
 }
 
 function CheckVerticalCollision() {
+    if (!currTetromino) {
+        startX = STARTX
+        startY = STARTY
+        CreateTetromino()
+        return true
+    }
+
     let tetrominoCopy = currTetromino
     let collision = false
 
@@ -203,15 +232,20 @@ function CheckVerticalCollision() {
             y++
         }
 
-        if (typeof stoppedShapeArray[x][y + 1] === 'string') {
-            DeleteTetromino()
-            startY++
-            DrawTetromino()
+        if (y >= 20) {
             collision = true
             break
         }
 
-        if (y >= 20) {
+        if (x >= 0 && y + 1 >= 0) {
+            if (typeof stoppedShapeArray[x][y + 1] === 'string') {
+                DeleteTetromino()
+                startY++
+                DrawTetromino()
+                collision = true
+                break
+            }
+        } else {
             collision = true
             break
         }
@@ -230,12 +264,14 @@ function CheckVerticalCollision() {
                 let x = square[0] + startX
                 let y = square[1] + startY
 
-                stoppedShapeArray[x][y] = currTetrominoColor
+                if (x >= 0 && y >= 0) {
+                    stoppedShapeArray[x][y] = currTetrominoColor
+                }
             }
 
             CheckForCompletedRows()
             CreateTetromino()
-            
+
             direction = DIRECTION.IDLE
             startX = 4
             startY = 0
@@ -244,7 +280,18 @@ function CheckVerticalCollision() {
     }
 }
 
+/**
+ * 
+ * @returns collision: boolean
+ */
 function CheckHorizontalCollision() {
+    if (!currTetromino) {
+        startX = STARTX
+        startY = STARTY
+        CreateTetromino()
+        return true
+    }
+
     let tetrominoCopy = currTetromino
     let collision = false
 
@@ -261,7 +308,7 @@ function CheckHorizontalCollision() {
 
         var stoppedShapeVal = stoppedShapeArray[x][y]
 
-        if (typeof stoppedShapeArray === 'string') {
+        if (typeof stoppedShapeVal === 'undefined' || typeof stoppedShapeVal === 'string') {
             collision = true
             break
         }
@@ -270,7 +317,18 @@ function CheckHorizontalCollision() {
     return collision
 }
 
+/**
+ * 
+ * @returns boolean: true if hit the wall
+ */
 function HittingWall() {
+    if (!currTetromino) {
+        startX = STARTX
+        startY = STARTY
+        CreateTetromino()
+        return true
+    }
+
     for (let i = 0; i < currTetromino.length; i++) {
         let newX = currTetromino[i][0] + startX
 
@@ -293,6 +351,7 @@ function CheckForCompletedRows() {
 
         for (let x = 0; x < gBArrayWidth; x++) {
             let square = stoppedShapeArray[x][y]
+
             if (square === 0 || (typeof square === 'undefined')) {
                 completed = false
                 break
@@ -338,39 +397,49 @@ function MoveAllRowsDown(rowsToDelete, startOfDeletion) {
     for (var i = startOfDeletion - 1; i >= 0; i--) { // Loop through rows from startOfDeletion-1 to 0
         for (var x = 0; x < gBArrayWidth; x++) { // Loop through each column in the current row
             var y2 = i + rowsToDelete; // Calculate the new row index
-            var square = stoppedShapeArray[x][i]; // Get the current square
-            var nextSquare = stoppedShapeArray[x][y2]; // Get the square at the new position
 
-            if (typeof square === 'string') { // If the current square is occupied
-                nextSquare = square; // Move the square to the new position
-                gameBoardArray[x][y2] = 1; // Update the game board array
-                stoppedShapeArray[x][y2] = square; // Update the stopped shape array
+            if (stoppedShapeArray[x][i]) { //check is not out of bounds
+                var square = stoppedShapeArray[x][i]; // Get the current square
+                var nextSquare = stoppedShapeArray[x][y2]; // Get the square at the new position
 
-                let coorX = coordinateArray[x][y2].x; // Get the x-coordinate for the new position
-                let coorY = coordinateArray[x][y2].y; // Get the y-coordinate for the new position
-                ctx.fillStyle = nextSquare; // Set the fill color to the square's color
-                ctx.fillRect(coorX, coorY, 21, 21); // Draw the square at the new position
+                if (typeof square === 'string') { // If the current square is occupied
+                    nextSquare = square; // Move the square to the new position
+                    gameBoardArray[x][y2] = 1; // Update the game board array
+                    stoppedShapeArray[x][y2] = square; // Update the stopped shape array
 
-                square = 0; // Clear the original square
-                gameBoardArray[x][i] = 0; // Update the game board array
-                stoppedShapeArray[x][i] = 0; // Update the stopped shape array
+                    let coorX = coordinateArray[x][y2].x; // Get the x-coordinate for the new position
+                    let coorY = coordinateArray[x][y2].y; // Get the y-coordinate for the new position
+                    ctx.fillStyle = nextSquare; // Set the fill color to the square's color
+                    ctx.fillRect(coorX, coorY, 21, 21); // Draw the square at the new position
 
-                coorX = coordinateArray[x][i].x; // Get the x-coordinate for the original position
-                coorY = coordinateArray[x][i].y; // Get the y-coordinate for the original position
-                ctx.fillStyle = 'white'; // Set the fill color to white
-                ctx.fillRect(coorX, coorY, 21, 21); // Clear the original square
+                    square = 0; // Clear the original square
+                    gameBoardArray[x][i] = 0; // Update the game board array
+                    stoppedShapeArray[x][i] = 0; // Update the stopped shape array
+
+                    coorX = coordinateArray[x][i].x; // Get the x-coordinate for the original position
+                    coorY = coordinateArray[x][i].y; // Get the y-coordinate for the original position
+                    ctx.fillStyle = 'white'; // Set the fill color to white
+                    ctx.fillRect(coorX, coorY, 21, 21); // Clear the original square
+                }
             }
         }
     }
 }
 
 function RotateTetromino() {
+    if (!currTetromino) {
+        startX = STARTX
+        startY = STARTY
+        CreateTetromino()
+        return
+    }
+
     let newRotation = new Array()
     let tetrominoCopy = currTetromino
     let currTetrominoBU //backup
 
     for (let i = 0; i < tetrominoCopy.length; i++) {
-        currTetromino = [...currTetromino]
+        currTetrominoBU = [...currTetromino]
         let x = tetrominoCopy[i][0]
         let y = tetrominoCopy[i][1]
         let newX = (GetLastSquareX() - y)
@@ -378,18 +447,27 @@ function RotateTetromino() {
         newRotation.push([newX, newY])
     }
 
-    DeleteTetromino()
-
-    try {
+    if(!CheckRotatedTetrominoOOB(newRotation)) {
+        DeleteTetromino()
         currTetromino = newRotation
         DrawTetromino()
-    } catch (e) {
-        if (e instanceof TypeError) {
-            currTetromino = currTetrominoBU
-            DeleteTetromino()
-            DrawTetromino()
-        }
     }
+}
+
+/**
+ * 
+ * @param {*} newRotation rotatedTetromino to check
+ * @returns true if block is out of bounds, false if is not
+ */
+function CheckRotatedTetrominoOOB(newRotation) {
+    for(let coords of newRotation) { //use 'of' instead of 'in', otherwise coords will be assumed type of string
+        if(coords[0] + startX < 0) return true
+        if(coords[1] + startY < 0) return true
+        if(coords[0] + startX >= gBArrayWidth) return true
+        if(coords[1] + startY >= gBArrayHeight) return true
+    }
+
+    return false
 }
 
 function GetLastSquareX() {
